1a3a2baf0106dd682f82f4080e544749de116e7c
Alexander Romero <alex1812r@gmail.com>
2023-03-06 20:28:39 -0400

    example actions

--- frontend/src/modules/users/views/UsersView.tsx
+++ frontend/src/modules/users/views/UsersView.tsx
@@ -1,9 +1,35 @@
+import { Button } from "@mui/material"
+import { useCallback, useEffect, useState } from "react"
 import { UsersTable } from "../components/UsersTable"
+import { getContratosAction } from "../users-action"
+
 
 export const UsersView = () => {
+  const [data, setData] = useState<any[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  
+  const getContratos = useCallback(() => {
+    setIsLoading(true)
+    getContratosAction()
+      .then((newData) => {
+        setData(newData)
+      })
+      .catch(() => {
+        // lanzar alerta (snack)
+      })
+      .finally(() =>{
+        setIsLoading(false)
+      })
+  }, []);
+
+  useEffect(() => {
+    getContratos()
+  }, [getContratos])
+  
   return (
     <div>
       <h1>Users</h1>
+      <Button onClick={getContratos}>Reload</Button>
       <UsersTable />
     </div>
   )
--- frontend/src/shared/api/Api.ts
+++ frontend/src/shared/api/Api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import { SERVER_URL } from "../constants/env.contants";
 
 export const Api = axios.create({
-  baseURL: `${SERVER_URL}/api`
-})
+  baseURL: `${SERVER_URL}/api`,
+});
