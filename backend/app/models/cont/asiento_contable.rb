b67e518a9dcc0e7881bb8164b39940d62b6297ba
Keiver Pacheco <krpachecop@gmail.com>
2023-03-09 10:39:46 -0400

    Se Arreglo la conexion del metodo Try en Asiento Contable

--- backend/app/models/cont/asiento_contable.rb
+++ backend/app/models/cont/asiento_contable.rb
@@ -1,39 +1,46 @@
 # Se define el módulo Cont
 module Cont
-    # Se define la clase AsientoContable que hereda de ApplicationRecord
-    class AsientoContable < ApplicationRecord
-      # Se establece la conexión a la base de datos
-      self.establish_connection(
-        adapter: 'oracle_enhanced',
-        encoding: 'utf8',
-        database: '//keruxdb:1521/PREPROD',
-        username: 'CONT',
-        password: 'CONT$P'
-      )
-  
-      # Se especifica el nombre de la tabla de la base de datos a utilizar
-      self.table_name = 'asientos_contables'
-      self.primary_key = "idasiento"
-      
-      # Asociaciones con otros modelos
-      belongs_to :Publicacion, foreign_key: "numpublicacion"
-      belongs_to :DocumentoOrigen, class_name: 'Doc::DocumentoOrigen', foreign_key: 'iddoc'
-      has_many :MovimientoContable, foreign_key: "idasiento"
-      
-      # Método para mostrar la referencia del documento
-      def dsp_RefDoc
-        Doc::DocumentoOrigen.try(:refdoc)
-      end
+  # Se define la clase AsientoContable que hereda de ApplicationRecord
+  class Cont::AsientoContable < ApplicationRecord
+    # Se establece la conexión a la base de datos
+    self.establish_connection(
+      adapter: "oracle_enhanced",
+      encoding: "utf8",
+      database: "//keruxdb:1521/PREPROD",
+      username: "CONT",
+      password: "CONT$P",
+    )
 
-      def dsp_otro
-        if Doc::DocumentoOrigen.respond_to?(:refdoc)
-            Doc::DocumentoOrigen.metodo
-        else
+    # Se especifica el nombre de la tabla de la base de datos a utilizar
+    self.table_name = "asientos_contables"
+    self.primary_key = "idasiento"
+
+    # Asociaciones con otros modelos
+    belongs_to :Publicacion, foreign_key: "numpublicacion"
+    #necesitamos asegurar de que la relación DocumentoOrigen
+    #siempre esté presente, incluso si el AsientoContable actual no tiene ningún DocumentoOrigen asociado.
+    #para hacer eso agregamos  el parametro optional: true
+    belongs_to :DocumentoOrigen, class_name: "Doc::DocumentoOrigen", foreign_key: "iddoc", optional: true
+    has_many :MovimientoContable, foreign_key: "idasiento"
+
+    # Método para mostrar la referencia del documento
+    def dsp_RefDoc
+      #asociamos la instancia con un self para que tome la relacion ya que se trabaja con modulos
+      self.DocumentoOrigen.try(:refdoc) || ""
+      #documento = self.DocumentoOrigen
+      #se utiliza el operador ternario (?) para comprobar si se obtuvo
+      #un objeto DocumentoOrigen. Si se obtuvo, se devuelve el valor de su atributo
+      #documento ? documento.refdoc : "Sin referencia de documento"
+    end
+
+    def dsp_otro
+      if Doc::DocumentoOrigen.respond_to?(:refdoc)
+          Doc::DocumentoOrigen.metodo
+      else
           puts "El método no está definido para este objeto"
-        end
-       # "Tipo de documento no especificado" unless defined?(Doc::TipoDocumento)
-       # Doc::TipoDocumento&.desctipodoc
       end
+      # "Tipo de documento no especificado" unless defined?(Doc::TipoDocumento)
+      # Doc::TipoDocumento&.desctipodoc
     end
   end
-  
+end
--- backend/app/models/doc/documento_origen.rb
+++ backend/app/models/doc/documento_origen.rb
@@ -1,43 +1,43 @@
 # Se define el módulo Cont
 module Doc
-     # Se define la clase AsientoContable que hereda de ApplicationRecord
-    class DocumentoOrigen < ApplicationRecord
-       #Se establece la conexión a la base de datos
-        self.establish_connection(
-          adapter: 'oracle_enhanced',
-          encoding: 'utf8',
-          database: '//keruxdb:1521/PREPROD',
-          username: 'DOC',
-          password: 'DOC$P'
-        )
+  # Se define la clase AsientoContable que hereda de ApplicationRecord
+  class DocumentoOrigen < ApplicationRecord
+    #Se establece la conexión a la base de datos
+    self.establish_connection(
+      adapter: "oracle_enhanced",
+      encoding: "utf8",
+      database: "//keruxdb:1521/PREPROD",
+      username: "DOC",
+      password: "DOC$P",
+    )
 
-        include MetodoGlobal
-    
-      # Configuración del archivo de configuración y otros atributos de  modelo
-      self.table_name = "documentos_origen"
-      self.primary_key = "iddoc"
-      
-      # Atributo virtual para almacenar errores personalizados
-      attr_accessor :error
-  
-      # Asociaciones con otros modelos
-      belongs_to :TipoDocumento, foreign_key: "tipodoc"
-      belongs_to :Beneficiario, foreign_key: "numbenef"
-      belongs_to :Moneda, class_name: "Kentron::Moneda", foreign_key: "codmoneda"
-      has_many :EventoAdmonUa, foreign_key: "iddoc"
-      has_many :EventoAdmon, foreign_key: "iddoc"
-      has_many :AsientoContable, class_name: "Cont::AsientoContable", foreign_key: "iddoc"
-      
-      # Método para mostrar el tipo de documento
-      def dsp_desctipodoc
-        desctipodoc = TipoDocumento.find(self.tipodoc)
-        desctipodoc.desctipodoc
-      end
+    include MetodoGlobal
 
-      def dsp_Desc
-        TipoDocumento.try(:desctipodoc)
-      end
-  
-      # Fin de la definición de la clase
+    # Configuración del archivo de configuración y otros atributos de  modelo
+    self.table_name = "documentos_origen"
+    self.primary_key = "iddoc"
+
+    # Atributo virtual para almacenar errores personalizados
+    attr_accessor :error
+
+    # Asociaciones con otros modelos
+    has_many :AsientoContable, class_name: "Cont::AsientoContable", foreign_key: "iddoc"
+    belongs_to :TipoDocumento, foreign_key: "tipodoc"
+    belongs_to :Beneficiario, foreign_key: "numbenef"
+    belongs_to :Moneda, class_name: "Kentron::Moneda", foreign_key: "codmoneda"
+    has_many :EventoAdmonUa, foreign_key: "iddoc"
+    has_many :EventoAdmon, foreign_key: "iddoc"
+
+    # Método para mostrar el tipo de documento
+    def dsp_desctipodoc
+      desctipodoc = TipoDocumento.find(self.tipodoc)
+      desctipodoc.desctipodoc
     end
+
+    def dsp_Desc
+      TipoDocumento.try(:desctipodoc)
+    end
+
+    # Fin de la definición de la clase
   end
+end
