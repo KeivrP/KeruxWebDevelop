46e701837f82daac56aa9938a878470fefc99c8b
Keiv <krpachecop@gmail.com>
2023-03-13 16:53:25 -0400

    Creacion de la primera pantalla del frontend con el primer controlador se agrego gema kaminari

--- backend/app/controllers/cont/asientos_contables_controller.rb
+++ backend/app/controllers/cont/asientos_contables_controller.rb
@@ -1,6 +1,49 @@
+# Controlador para la tabla Asientos Contables en el namespace Cont
 class Cont::AsientosContablesController < ApplicationController
+  before_action :asiento_find, only: [:show]
+
+  # Acción index para listar los registros de la tabla
   def index
-    asiento1 = Cont::AsientoContable.first
-    render json: asiento1.as_json
+    # Seleccionar los campos deseados de la tabla y paginar el resultado
+    asiento = Cont::AsientoContableUa
+      .select(:iddoc, :descasiento, :refdoc, :anocont, :percont, :fecasiento, :numpublicacion, :stsasiento)
+      .where(stsasiento: ["REC", "RCH"])
+      .page(params[:page])
+      .per(10)
+    # Renderizar los resultados como JSON, incluyendo los métodos por defecto
+    render json: asiento.as_json #(methods: json_default_methods)
+  end
+
+  # Acción show para mostrar un registro específico de la tabla
+  def show
+    # Renderizar el registro encontrado como JSON, incluyendo los métodos por defecto
+    render json: @asiento.as_json #(methods: json_default_methods)
+  end
+
+  def update
+    @asiento.fechar = newfecha
+    @asiento.update
+  end
+
+  #Metodos que solo puedo usar en el controlador
+  private
+
+  # Método privado para encontrar un registro por su ID
+  def asiento_find
+    #@ es un variable que puedo usar en toda la instancia del controlador
+
+    @asiento = Cont::AsientoContableUa.where(iddoc: params[:iddoc])
+  end
+
+  def documento_find
+    @documento = Doc::DocumentoOrigen.find(iddocparams[:iddoc])
+  end
+
+  # Definir los métodos por defecto a incluir en el resultado JSON
+  def json_default_methods
+    %i[
+      dsp_FecRef,
+      dsp_CodSisReg,
+    ]
   end
 end
--- frontend/src/modules/cont/views/ContView.tsx
+++ frontend/src/modules/cont/views/ContView.tsx
@@ -1,22 +1,110 @@
-import { useEffect } from "react";
+import { Box, TablePagination } from "@mui/material";
+import React, { useEffect, useState, useRef } from "react";
+import CustomTable from "../../../shared/components/table/CustomTable";
+import Header from "../../../shared/layout/header";
 import { useAppSelector, useAppDispatch } from "../../../store/hooks";
 import { getContAction } from "../cont-actions";
+import '../../../shared/layout/styles/styles.css'
+import ButtonSearch from "../../../shared/components/button/ButtonSearch";
+import { IconButtonMenu } from "../../../shared/components/button/IconButtonMenu";
+
 
 export const ContView = () => {
   const seat = useAppSelector((state) => state.cont.seat)
   const loadingSeat = useAppSelector((state) => state.cont.loadingSeat)
+
   const dispatch = useAppDispatch();
+
+  const [page, setPage] = React.useState(2);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const goBack = () => {
+    // lógica del botón
+  };
 
   useEffect(() => {
     dispatch(getContAction())
+
   }, [])
+  
+
+  const columns = [
+    { id: 'iddoc', label: 'Id Doc.', width: '100px' },
+    { id: 'd', label: 'Descripción', width: '200px' },
+    { id: 'dsp_RefDoc', label: 'Referencia', width: '100px' },
+    { id: 'cicty', label: 'Año', width: '100px' },
+    { id: 'cdity', label: 'Período', width: '100px' },
+    { id: 'cifty', label: 'Fecha', width: '100px' },
+    { id: 'city', label: 'Publicación', width: '100px' },
+    { id: 'name', label: 'Monto', width: '100px' },
+    { id: 'city', label: 'Estatus', width: '100px' },
+  ];
+
+  const allRows = [
+    { name: 'Juan', age: 30, city: 'Buenos Aires' },
+    { name: 'Maria', age: 25, city: 'Madrid' },
+    { name: 'Peter', age: 40, city: 'New York' },
+    { name: 'Sara', age: 35, city: 'Berlin' },
+  ];
+  /* 
+    const filteredRows = rows.filter((row) =>
+    Object.values(row).some(
+      (value) => value.toString().toLowerCase().indexOf(filterValue.toLowerCase()) !== -1
+    )
+  ); */
 
   return (
-    <div>
-      {loadingSeat ? 'loading...' : null}
+    <>
+      <Header headerTitle="Asientos por codificar" buttonAction={goBack} />
+      <div className="my-div">
+        <Box
+          height={40} // Establece la altura en 100px
+          bgcolor="#FFFFFF"
+          border={1}
+          borderColor="#FFFFFF"
+          boxShadow={3}
+          borderRadius={1}
+        >
+          <ButtonSearch />
+         
+          {/*  <IconButtonMenu icon={mmmm} /> */}
+        </Box>
+        {seat && <CustomTable columns={columns} rows={seat} headerColumns={['iddoc']} maxChar={20} />}
+        <div className="my-pagination">
+          <Box flex="1" />
+          <Box>
+            <TablePagination
+              component="div"
+              count={100}
+              page={page}
+              onPageChange={handleChangePage}
+              rowsPerPage={rowsPerPage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              rowsPerPageOptions={[10, 25, 50, 100]}
+              labelRowsPerPage="Filas por página"
+              labelDisplayedRows={({ from, to, count }) => `${from}-${to} de ${count}`}
+            />
+          </Box>
+        </div>
+
+      </div>
+      {/* {loadingSeat ? 'loading...' : null}
       <pre>
         {JSON.stringify(seat)}
-      </pre>
-    </div>
+      </pre> */}
+    </>
   )
 }
